80724928ca90f5e5452f022d7ad403b5e0bb56e6
Seny Irinarkhova <kseniiaIrinarkhova@gmail.com>
2024-02-05 13:43:55 -0800

    object structure

--- src/index.js
+++ src/index.js
@@ -8,3 +8,44 @@ function getUserData(id) {
         db3: db3
     };
 }
+
+console.log(central(1));
+
+
+class Company{
+    constructor(company){
+        this.name = company.name
+        this.catchPhrase = company.catchPhrase
+        this.bs = company.bs
+    }
+}
+class Geo{
+    constructor(lat, lng){
+        this.lat = lat
+        this.lng = lng
+    }
+}
+
+
+class Address{
+    constructor(address){
+        this.street = address.street
+        this.suite = address.suite
+        this.city = address.city
+        this.zipcode = address.zipcode
+        this.geo = new Geo(address.geo.lat, address.geo.lng)
+    }
+}
+
+class Person {
+    constructor(id, name, username, email, address, phone, website,company){
+        this.id = id
+        this.name = name
+        this.username = username
+        this.email = email
+        this.address = new Address(address)
+        this.phone = phone
+        this.website = website
+        this.company = new Company(company)
+    }
+}
